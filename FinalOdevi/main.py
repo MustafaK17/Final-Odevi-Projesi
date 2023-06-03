685f65646df53f5d47dc337c6400bf127da37ea7
MustafaK17 <mustafakarademir2004@gmail.com>
2023-06-03 19:45:25 +0300

    insan sınıfı eklendi
    insan sınıfı çalıştığını test etmek için main.py dosyasına birkaç kod ekledim

--- FinalOdevi/main.py
+++ FinalOdevi/main.py
@@ -4,3 +4,9 @@ from calisan import calisan
 from maviyaka import maviyaka
 from beyazyaka import beyazyaka
 import pandas as pd
+
+insan1 = insan("11223344556", "Kerem", "Yılmaz", 30, "Erkek", "Türk")
+insan2 = insan("12312312345", "Selin", "Kaya", 27, "Kadın", "Türk")
+
+print(insan1)
+print(insan2)
